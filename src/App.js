ef87ac423a4a60466cdd1688c3eb53f714ed7fa6
Jose Martinez <josmmartinez94@gmail.com>
2018-10-09 15:22:07 -0700

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import './CSS/App.css';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
       </div>
     );
   }
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./CSS/Navbar.css"
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div className="navbar-container">
+        <div>
+            Ultimate Movie Guide
+        </div>
+      </div>
+    )
+  }
+}
