8aaf023d7435d86550fe86f5aa13c981a1486299
Jose Martinez <josmmartinez94@gmail.com>
2018-10-09 15:44:01 -0700

    Add movieDB image attribution

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import "./CSS/Navbar.css"
+import powerBy from './images/power-by-movie.png';
 
 export default class Navbar extends Component {
   render() {
     return (
       <div className="navbar-container">
-        <div>
-            Ultimate Movie Guide
+        <div className="navbar">
+            <img src={powerBy} alt="powered by movieDB"/>
         </div>
       </div>
     )
