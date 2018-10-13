eb6cd4c36e106d0d09ac403ebbc7308bca68395e
Jose Martinez <josmmartinez94@gmail.com>
2018-10-13 13:55:12 -0700

    Change date format

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import './CSS/App.css';
 import Navbar from './Navbar';
 import MovieIndex from './components/movie_index'
+import { Switch, Route } from 'react-router' 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar />
-        <MovieIndex />
+      <Switch>
+        <Route path="/" component={MovieIndex} />
+      </Switch>
       </div>
     );
   }
--- src/components/movie_index.jsx
+++ src/components/movie_index.jsx
@@ -12,7 +12,19 @@ class MovieIndex extends Component {
     }
 
     popularMovies() {
-        const urlString = `https://api.themoviedb.org/3/movie/now_playing?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
+        const popularString = `https://api.themoviedb.org/3/movie/now_playing?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
+        const playingString = `https://api.themoviedb.org/3/movie/now_playing?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
+        const topRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
+
+        let urlString;
+        if (this.props.location.pathname === "/rated") {
+            urlString = topRated;
+        } else if (this.props.location.pathname === "/playing") {
+            urlString = playingString;
+        } else {
+            urlString = popularString;
+        }
+
         $.ajax({
             url: urlString,
             success: (popularMovies) => {
@@ -28,16 +40,44 @@ class MovieIndex extends Component {
         })
     }
 
+    displayDate() {
+        let months = {
+            "01" : "Jan.",
+            "02" : "Feb.",
+            "03" : "Mar.",
+            "04" : "Apr.",
+            "05" : "May",
+            "06" : "Jun.",
+            "07" : "Jul.",
+            "08" : "Aug.",
+            "09" : "Sept.",
+            "10" : "Oct.",
+            "11" : "Nov.",
+            "12" : "Dec."
+        }
+        this.state.movieRows.forEach(movie => {
+            let year = "";
+            let month = "";
+            let day = "";
+            let date = movie.release_date.split('-')
+            year += date[0];
+            month += months[date[1]];
+            day += date[2]
+            movie.get_date = `${month} ${day}, ${year}`
+        })
+    }
+
   render() {
     return (
       <div className="movie-index-container">
         <div className="grid-container">
         {this.popularMovies()}
+        {this.displayDate()}
             {this.state.movieRows.map(movie => <li key={movie.id} className="movie">
                 <img src={movie.img} alt="Movie poster"/>
                 <div className="movie-info">
                     <div className="movie-title">{movie.title}</div>
-                    <div className="movie-date">Release Date: {movie.release_date}</div>
+                    <div className="movie-date">Release Date: {movie.get_date}</div>
                     <p className="movie-overview">{movie.overview}</p>
                 </div>
             </li>)}
