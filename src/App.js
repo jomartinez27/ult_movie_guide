2460c2bca197369bafc263bb2a30cc3c6e057ff8
Jose Martinez <josmmartinez94@gmail.com>
2018-10-18 09:29:54 -0700

    Fix api call

--- src/App.js
+++ src/App.js
@@ -1,61 +1,26 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom'
-import './CSS/App.css';
-import MovieRow from './components/movie_index'
-import NavBar from './Navbar'
-import $ from 'jquery'
+import { Route, Switch } from 'react-router-dom'
+import MovieIndex from './components/MovieIndexContainer'
+import MovieRated from './components/MovieIndexRatedContainer'
+import MoviePlaying from './components/MovieIndexPlayingContainer'
+import Home from './components/HomeContainer'
+import MovieDetail from './components/MovieDetailContainer.jsx'
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-
-  displayMovies() {
-    const topRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
-    const nowPlaying = `https://api.themoviedb.org/3/movie/now_playing?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
-    const popular = `https://api.themoviedb.org/3/movie/popular?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
-
-    let urlString;
-
-    if (this.props.location.pathname === "/rated") {
-      urlString = topRated
-    } else if (this.props.location.pathname === "/playing") {
-      urlString = nowPlaying
-    } else {
-      urlString = popular
-    }
-
-
-    $.ajax({
-      url: urlString,
-      method: 'GET',
-      success: (searchResults) => {
-        const movies = searchResults.results
-        let movieRows = []
-
-        movies.forEach(movie => {
-          movie.img = "https://image.tmdb.org/t/p/w200" + movie.poster_path
-          movie.background = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path
-          const movieRow = <MovieRow key={movie.id} movie={movie}/>
-          movieRows.push(movieRow)
-        })
-
-        this.setState({rows: movieRows})
-      }
-    })
-  }
-
   render() {
     return (
       <div>
-        <NavBar />
-        {this.displayMovies()}
-        {this.state.rows}
+        <Switch>
+          <Route exact path="/popular" component={MovieIndex} />
+          <Route exact path="/rated" component={MovieRated} />
+          <Route exact path="/playing" component={MoviePlaying} />
+          <Route exact path="/movies/:movieId" component={MovieDetail} />
+          <Route path="/" component={Home} />
+        </Switch>
       </div>
     );
   }
 }
 
-export default withRouter(App);
+export default App;
+
--- src/util/api_util.js
+++ src/util/api_util.js
@@ -3,7 +3,7 @@ import $ from 'jquery'
 export const fetchPopular = () => (
     $.ajax({
         method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/popular?api_key=13974aec8a507bc409f142057852e657&language=en-US&page=1`,
+        url: `https://api.themoviedb.org/3/movie/popular?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
     })
 )
 
@@ -14,16 +14,23 @@ export const fetchPlaying = () => (
     })
 )
 
-export const fetchRated = () => (
+export const fetchTopRated = () => (
     $.ajax({
         method: 'GET',
         url: `https://api.themoviedb.org/3/movie/top_rated?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
     })
 )
 
-export const fetchMovie = id => (
+export const fetchMovie = movie_id => (
     $.ajax({
         method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/${id}?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US`
+        url: `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US`,
+    })
+)
+
+export const fetchCast = movieId => (
+    $.ajax({
+        method: 'GET',
+        url: `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=13974aec8a507bc409f142057852e657`
     })
 )
