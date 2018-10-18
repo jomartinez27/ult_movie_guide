a178bb6e12345d0cebcce0527cd45fe6d77a8d65
Jose Martinez <josmmartinez94@gmail.com>
2018-10-18 09:49:26 -0700

    Fix navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,23 +4,23 @@ import "./CSS/Navbar.css"
 import powerBy from './images/power-by-movie.png';
 
 export default class Navbar extends Component {
-    
-  render() {
-    return (
-      <div className="navbar-container">
-        <div className="navbar">
-            <Link to="/"><img src={powerBy} alt="powered by movieDB"/></Link>
-        </div>
-            <div className="links-container">
-                <div className="links">
-                    <ul>
-                        <li><Link to="/popular">Popular</Link></li>
-                        <li><Link to="/rated">Top Rated</Link></li>
-                        <li><Link to="/playing">Now Playing</Link></li>
-                    </ul>
+    render() {
+        return (
+            <div className="navbar-container">
+                <div className="navbar">
+                    <Link to="/"><img src={powerBy} alt="powered by movieDB" /></Link>
+                </div>
+                <div className="links-container">
+                    <div className="links">
+                        <ul>
+                            <li className="link-list"><Link to="/">Home</Link></li>
+                            <li className="link-list"><Link to="/popular">Popular</Link></li>
+                            <li className="link-list"><Link to="/rated">Top Rated</Link></li>
+                            <li className="link-list"><Link to="/playing">Now Playing</Link></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-      </div>
-    )
-  }
+        )
+    }
 }
