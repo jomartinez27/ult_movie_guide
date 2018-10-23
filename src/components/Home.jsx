06ebdaa15c4812bb21be8419eb02825fe67316a6
Jose Martinez <josmmartinez94@gmail.com>
2018-10-22 23:14:56 -0700

    Add carousel to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../Navbar'
 import { Link } from 'react-router-dom'
+import { Carousel } from 'react-bootstrap'
 import "../CSS/Home.css"
 
 class Home extends React.Component {
@@ -18,13 +19,35 @@ class Home extends React.Component {
                 <Navbar />
                 <div className="movie-guide">
                     The Ultimate Movie Guide
-        </div>
+                </div>
                 <div className="home-movies-container">
-                    {this.props.movies.map(movie =>
-                        <Link to={`/movies/${movie.id}`} key={movie.id} className="movie-container">
-                            <img src={"https://image.tmdb.org/t/p/w200" + movie.poster_path} alt="Movie poster" />
-                        </Link>
-                    )}
+                    <Carousel>
+                        <Carousel.Item>
+                            <Link to={`movies/${this.props.movies[0].id}`}>
+                            <img src={"https://image.tmdb.org/t/p/original" + this.props.movies[0].poster_path} alt="Movie poster" className="home-image"/>
+                            </Link>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Link to={`movies/${this.props.movies[1].id}`}>
+                            <img src={"https://image.tmdb.org/t/p/original" + this.props.movies[1].poster_path} alt="Movie poster" className="home-image"/>
+                            </Link>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Link to={`movies/${this.props.movies[2].id}`}>
+                            <img src={"https://image.tmdb.org/t/p/original" + this.props.movies[2].poster_path} alt="Movie poster" className="home-image"/>
+                            </Link>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Link to={`movies/${this.props.movies[3].id}`}>
+                                <img src={"https://image.tmdb.org/t/p/original" + this.props.movies[3].poster_path} alt="Movie poster" className="home-image"/>
+                            </Link>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Link to={`movies/${this.props.movies[4].id}`}>
+                                <img src={"https://image.tmdb.org/t/p/original" + this.props.movies[4].poster_path} alt="Movie poster" className="home-image"/>
+                            </Link>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
             </div>
         )
