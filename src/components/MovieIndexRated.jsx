9e303f2578b3fe8e56000d892d22c708acc99f84
Jose Martinez <josmmartinez94@gmail.com>
2018-10-29 23:37:11 -0700

    Add bootstrap min to index, refactor component

--- src/components/MovieIndexRated.jsx
+++ src/components/MovieIndexRated.jsx
@@ -2,37 +2,11 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../CSS/Movies.css'
 import Navbar from '../Navbar';
+import Movies from './Movies'
 
 class MovieIndexRated extends React.Component {
     componentDidMount() {
         this.props.fetchTopRated()
-    }
-
-    displayDate() {
-        let months = {
-            "01": "Jan.",
-            "02": "Feb.",
-            "03": "Mar.",
-            "04": "Apr.",
-            "05": "May",
-            "06": "Jun.",
-            "07": "Jul.",
-            "08": "Aug.",
-            "09": "Sept.",
-            "10": "Oct.",
-            "11": "Nov.",
-            "12": "Dec."
-        }
-        this.props.movies.forEach(movie => {
-            let year = "";
-            let month = "";
-            let day = "";
-            let date = movie.release_date.split('-')
-            year += date[0];
-            month += months[date[1]];
-            day += date[2]
-            movie.get_date = `${month} ${day}, ${year}`
-        })
     }
 
     render() {
@@ -47,23 +21,9 @@ class MovieIndexRated extends React.Component {
             )
         }
         return (
-            <div>
-                <Navbar />
-                <div className="movie-index-container">
-                    <div className="location">Top Rated</div>
-                    <div className="grid-container">
-                        {this.displayDate()}
-                        {this.props.movies.map(movie => <li key={movie.id} className="movie">
-                            <Link to={`movies/${movie.id}`}><img src={"https://image.tmdb.org/t/p/w200" + movie.poster_path} alt="Movie poster" /></Link>
-                            <div className="movie-info">
-                                <Link to={`movies/${movie.id}`}><div className="movie-title">{movie.title}</div></Link>
-                                <div className="movie-date">Release Date: {movie.get_date}</div>
-                                <p className="movie-overview">{movie.overview}</p>
-                            </div>
-                        </li>)}
-                    </div>
-                </div>
-            </div>
+          <div>
+            <Movies movies={this.props.movies}/>
+          </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Root from './components/Root';
 import configureStore from './store/store'
 
@@ -8,5 +9,5 @@ import configureStore from './store/store'
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     let store = configureStore();
-    ReactDOM.render(<Root store={store}/>, root) 
+    ReactDOM.render(<Root store={store}/>, root)
 })
