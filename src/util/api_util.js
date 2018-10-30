dc3791397e01953190f6568bf85b7908063f52bf
Jose Martinez <josmmartinez94@gmail.com>
2018-10-30 12:38:49 -0700

    Refactor

--- src/util/api_util.js
+++ src/util/api_util.js
@@ -1,36 +1,37 @@
 import $ from 'jquery'
+const { apiKey } = require('../config.js')
 
 export const fetchPopular = () => (
     $.ajax({
         method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/popular?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
+        url: `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`
     })
 )
 
 export const fetchPlaying = () => (
     $.ajax({
         method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/now_playing?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
+        url: `https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US&page=1`
     })
 )
 
 export const fetchTopRated = () => (
     $.ajax({
         method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/top_rated?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US&page=1`
+        url: `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&language=en-US&page=1`
     })
 )
 
 export const fetchMovie = movie_id => (
     $.ajax({
         method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${'13974aec8a507bc409f142057852e657'}&language=en-US`,
+        url: `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${apiKey}&language=en-US`,
     })
 )
 
 export const fetchCast = movieId => (
     $.ajax({
         method: 'GET',
-        url: `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=13974aec8a507bc409f142057852e657`
+        url: `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`
     })
 )
