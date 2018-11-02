72be08d5cc02f8bf3a67a53c1b02d6cb5cc16c5c
Jose Martinez <josmmartinez94@gmail.com>
2018-11-01 20:54:49 -0700

    Introduce build

---
+++ build/precache-manifest.635a4d8c71110489dc84d0b9c6468995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2b9726916837df2469b4",
+    "url": "/static/js/main.2b972691.chunk.js"
+  },
+  {
+    "revision": "4caec29a8ba6cbe63f1b",
+    "url": "/static/js/1.4caec29a.chunk.js"
+  },
+  {
+    "revision": "2b9726916837df2469b4",
+    "url": "/static/css/main.321fe71e.chunk.css"
+  },
+  {
+    "revision": "4caec29a8ba6cbe63f1b",
+    "url": "/static/css/1.15116e9d.chunk.css"
+  },
+  {
+    "revision": "c3e6b5877d7b98d454242c5f6415cc63",
+    "url": "/index.html"
+  }
+];
